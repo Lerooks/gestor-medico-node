850018402a202bebadda3aa805f1d7747a06edd8
Daniel Machado <daniel.machado2>
2021-06-21 12:53:59 -0300

    feat: add protect all routers in appoiment and patient
    - add middleware auth in appointment

--- src/routes/patientRouter.js
+++ src/routes/patientRouter.js
@@ -1,13 +1,14 @@
 const express = require("express");
+
+// patient
 const patientController = require("../controllers/patientController");
 const patientRouter = express.Router();
 
+// middleware
+const auth = require('../middlewares/auth')
 
-patientRouter.post("/newPatient",
-    patientController.newPatient);
-patientRouter.post("/searchPatientByName",
-    patientController.searchPatientByName);
-patientRouter.put("/updatePatient",
-    patientController.updatePatient);
+patientRouter.post("/newPatient", auth, patientController.newPatient);
+patientRouter.post("/searchPatientByName", auth, patientController.searchPatientByName);
+patientRouter.put("/updatePatient", auth, patientController.updatePatient);
 
 module.exports = patientRouter;
